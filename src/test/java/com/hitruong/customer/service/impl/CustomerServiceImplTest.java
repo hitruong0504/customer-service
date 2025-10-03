bbd1a366369e118c5c83f14e169de72cc51bd810
hitruong <hitruong.work@gmail.com>
2025-10-03 21:19:43 +0700

    feat: add unit test

---
+++ src/test/java/com/hitruong/customer/service/impl/CustomerServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.hitruong.customer.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceImplTest {
+
+    @Test
+    void createCustomer() {
+    }
+
+    @Test
+    void getCustomer() {
+    }
+}
