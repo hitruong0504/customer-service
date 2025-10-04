947ae8d8f391e8e4949812a513a36b1a215a8f9c
HiTruong <72957243+hitruong0504@users.noreply.github.com>
2025-10-04 11:19:08 +0700

    Feature/crud api customer (#6)
    * update api crud
    
    * update maven workflow
    
    * update exception handler
    
    * feat: add unit test
    
    * disabled contextLoads
    
    * disabled context loading
    
    * feat: update crud api cont
    
    * feat: update testing api
    
    * feat: add field isDeleted and re-handle process related
    
    * feat: update unit test
    
    ---------
    
    Co-authored-by: hitruong <hitruong.work@gmail.com>

--- src/main/java/com/hitruong/customer/repository/CustomerRepository.java
+++ src/main/java/com/hitruong/customer/repository/CustomerRepository.java
@@ -13,9 +13,9 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END " +
             "FROM Customer c " +
-            "WHERE c.email = :#{#email}")
+            "WHERE c.email = :#{#email} AND c.isDeleted = false")
     boolean existsByEmail(@Param("email") String email);
 
-    @Query("SELECT c FROM Customer c WHERE c.email = :email AND c.id = :id")
+    @Query("SELECT c FROM Customer c WHERE c.email = :email AND c.id = :id AND c.isDeleted = false")
     Optional<Customer> findByEmailAndId(@Param("email") String email, @Param("id") Long id);
 }
